adadbd7bf378b0bdcfabda11e95cdba5ca67da3d
daniel-herencia <daniherencia@gmail.com>
2021-11-20 15:35:52 +0100

    Change2

--- ParteWebpagedjango/blog/views.py
+++ ParteWebpagedjango/blog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog MECHÓ</h1>')
+    return HttpResponse('<h1>Blog MECHoooo</h1>')
 
 def about(request):
     return HttpResponse('<h1>Blog About</h1>')
