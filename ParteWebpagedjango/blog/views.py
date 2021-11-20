cefc560a58f4b17915da7e10a7e8d080a43ad526
daniel-herencia <daniherencia@gmail.com>
2021-11-20 15:33:38 +0100

    first commit

---
+++ ParteWebpagedjango/blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse('<h1>Blog MECHÓ</h1>')
+
+def about(request):
+    return HttpResponse('<h1>Blog About del Belloto</h1>')
+    
+def prueba2(request):
+    return HttpResponse('<h1>PAGINA PARA PROBAR</h1>')
+    
