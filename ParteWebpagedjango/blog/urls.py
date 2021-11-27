3097d668e7de0ea2863b5d782a28ebd63d36f98b
daniel-herencia <daniherencia@gmail.com>
2021-11-27 12:17:55 +0100

    Video 3

--- ParteWebpagedjango/blog/urls.py
+++ ParteWebpagedjango/blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
-    path('prueba2/', views.about, name='blog-pureba2'),
+    #path('prueba2/', views.prueba2, name='blog-pureba2'),
 ]
--- ParteWebpagedjango/blog/views.py
+++ ParteWebpagedjango/blog/views.py
@@ -2,12 +2,35 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+#Posts of the blog (Dictionaries)
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
+
 def home(request):
-    return HttpResponse('<h1>Blog MECHooooo</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)    #To return a template rendered
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
-    
-def prueba2(request):
-    return HttpResponse('<h1>PAGINA PARA PROBAR</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})   #title => tab title 
+
+
+
+#def prueba2(request):
+#   return HttpResponse('<h1>PAGINA PARA PROBAR</h1>')
     
