327ac5b2b316ad7b2ed8f8ad6bf03334ad8dc101
daniel-herencia <daniherencia@gmail.com>
2021-12-08 15:43:52 +0100

    Video 5

--- ParteWebpagedjango/blog/views.py
+++ ParteWebpagedjango/blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
 #Create dictionaries with the posts of the blog
+"""#No longer needed, we now how to create them
 posts = [
     {
         'author': 'CoreyMS',
@@ -18,11 +20,11 @@ posts = [
         'date_posted': 'August 28, 2018'
     }
 ]
-
+"""
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)    #render(request, html template, variables) => To return a template rendered
 
