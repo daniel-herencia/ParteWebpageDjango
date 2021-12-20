1c7c77e5fc4f002155a14b5955d376034a5be305
daniel-herencia <daniherencia@gmail.com>
2021-12-20 09:04:10 +0100

    video7

--- ParteWebpagedjango/ParteWebpagedjango/settings.py
+++ ParteWebpagedjango/ParteWebpagedjango/settings.py
@@ -130,6 +130,9 @@ STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- ParteWebpagedjango/users/views.py
+++ ParteWebpagedjango/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 def register(request):
@@ -8,12 +9,16 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account creted for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
+
+@login_required     #To avoid not login users to enter in a profile directly with the link
+def profile(request):
+    return render(request, 'users/profile.html')
 
 #Kinds of message: 
 """
