539ae99bbd0041b97bc03529cb4549fe51a2927c
daniel-herencia <daniherencia@gmail.com>
2021-12-20 20:24:11 +0100

    video 8 y 9

--- ParteWebpagedjango/ParteWebpagedjango/settings.py
+++ ParteWebpagedjango/ParteWebpagedjango/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from decouple import config #for enviroment variables and secret keys
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,6 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
