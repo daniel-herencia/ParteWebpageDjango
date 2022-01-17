33c89395eabc9661fadcf81f2dcbaaada2e26147
daniel-herencia <daniherencia@gmail.com>
2022-01-17 16:47:58 +0100

    Tutorial finished

--- ParteWebpagedjango/ParteWebpagedjango/settings.py
+++ ParteWebpagedjango/ParteWebpagedjango/settings.py
@@ -137,6 +137,13 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config("USER_MAIL")
+EMAIL_HOST_PASSWORD = config("MAIL_KEY")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
