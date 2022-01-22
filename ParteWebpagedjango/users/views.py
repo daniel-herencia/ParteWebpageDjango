9c622c97b9f3a6497e96beb3ae723ea50ccfc3d2
daniel-herencia <daniherencia@gmail.com>
2022-01-22 18:56:59 +0100

    delete message import

--- ParteWebpagedjango/users/views.py
+++ ParteWebpagedjango/users/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
@@ -9,7 +8,6 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
         form = UserRegisterForm()
@@ -26,7 +24,6 @@ def profile(request):
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f'Your account has been updated!')
             return redirect('profile')
 
     else:
